b900428eb3e904d4c364a683c8995b85c6238145
Rebrin <rebrin.na@gmail.com>
2023-10-20 15:50:35 +0600

    Create fox_run_animation

---
+++ Assets/Scripts/Press.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Press : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnMouseDown()
+    {
+        _animator.SetTrigger("start_run");
+    }
+}
